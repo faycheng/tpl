bad90108753355a5d1b09d02ded2591832660e93
faycheng <fei.cheng@daocloud.io>
2017-09-07 15:56:07 +0800

    add construct_context_from_py

--- tpl/constructor.py
+++ tpl/constructor.py
@@ -13,14 +13,24 @@ from tpl import errors
 from tpl import sandbox
 
 
-
 def construct_context_from_shell(source):
     assert os.path.exists(source) and os.path.isfile(source)
     command = 'source {}'.format(source)
     return sandbox.shell_execute(command)
 
 
-def construct_context_from_py():
+def construct_context_from_py(source):
+    assert os.path.exists(source) and os.path.isfile(source)
+    injected_locals = {}
+    source_name = source.split('/')[-1].split('.')[0]
+    sys.path.insert(0, path.get_parent_path(source, 1))
+    injected_locals.setdefault('sys', sys)
+    try:
+        context = sandbox.py_execute('from {} import construct;context=construct()'.format(source_name), injected_locals=injected_locals)
+    finally:
+        sys.path.pop()
+    return context
+
     pass
 
 
