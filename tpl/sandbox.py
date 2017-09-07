aacb967a08f35ab23bdee778dc18c7bc8f07f721
faycheng <fei.cheng@daocloud.io>
2017-09-07 14:46:12 +0800

    add sandbox environment for executing py constructor

---
+++ tpl/sandbox.py
@@ -0,0 +1,56 @@
+# -*- coding:utf-8 -*-
+
+import os
+import sys
+
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+import uuid
+import json
+import threading
+from tpl import path
+from tpl import prompt
+
+
+DEFAULT_GLOBALS = {
+    'os': os,
+    'sys': sys,
+    'json': json,
+    'path': path,
+    'mdfifo': os.mkfifo,
+    'prompt': prompt
+}
+
+DEFAULT_LOCALS = {
+    'context_pipe': None
+}
+
+
+class ExecThread(threading.Thread):
+    def __init__(self, command, injected_globals, injected_locals):
+        self.command = command
+        self.injected_globals = injected_globals
+        self.injected_locals = injected_locals
+        super(ExecThread, self).__init__(name='exec_tpl_constructor')
+
+    def run(self):
+        exec(self.command, self.injected_globals, self.injected_locals)
+
+
+def execute(command, injected_globals=None, injected_locals=None):
+    injected_globals = injected_globals or {}
+    injected_locals = injected_locals or {}
+    injected_globals.update(DEFAULT_GLOBALS)
+    pipe_path = '/tmp/{}.pipe'.format(str(uuid.uuid4()))
+    os.mkfifo(pipe_path)
+    DEFAULT_LOCALS['context_pipe'] = pipe_path
+    injected_locals.update(DEFAULT_LOCALS)
+    command = 'pipe=open(context_pipe, "w");pipe.write(json.dumps({}));pipe.close()'.format(command)
+    ExecThread(command, injected_globals, injected_locals).start()
+    pipe = open(pipe_path)
+    context = json.loads(pipe.read())
+    pipe.close()
+    return context
+
+
+print(execute('{"key":"value"}'))
