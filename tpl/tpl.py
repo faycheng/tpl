592678144f5d9672a3a56504f8ccad28c81973d4
faycheng <fei.cheng@daocloud.io>
2017-09-07 17:19:56 +0800

    add Template

---
+++ tpl/tpl.py
@@ -0,0 +1,32 @@
+# -*- coding:utf-8 -*-
+
+import os
+import sys
+
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+import jinja2
+from tpl import path
+from tpl import errors
+
+
+class Template(object):
+    IGNORE_FILES = [
+        'construct.sh',
+        'construct.py'
+    ]
+
+    def __init__(self, tpl_dir):
+        self.tpl_dir = tpl_dir
+
+    def is_ignored_file(self, file):
+        pass 
+
+    def render_file(self, file):
+        pass
+
+    def render_dir(self, dir):
+        pass
+
+    def render(self, context):
+        pass
