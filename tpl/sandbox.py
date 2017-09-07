914b7b5bf1b5c898ccc1dae3a39cb63962eaab7d
faycheng <fei.cheng@daocloud.io>
2017-09-07 16:22:38 +0800

    update temp pipe context

--- tpl/sandbox.py
+++ tpl/sandbox.py
@@ -47,15 +47,12 @@ def py_execute(command, injected_globals=None, injected_locals=None):
     injected_globals = injected_globals or {}
     injected_locals = injected_locals or {}
     injected_globals.update(DEFAULT_PY_GLOBALS)
-    pipe_path = '/tmp/{}.pipe'.format(str(uuid.uuid4()))
-    os.mkfifo(pipe_path)
-    DEFAULT_PY_LOCALS['context_pipe'] = pipe_path
-    injected_locals.update(DEFAULT_PY_LOCALS)
-    command = '{};pipe=open(context_pipe, "w");pipe.write(json.dumps(context));pipe.close()'.format(command)
-    PyExec(command, injected_globals, injected_locals).start()
-    pipe = open(pipe_path)
-    context = json.loads(pipe.read())
-    pipe.close()
+    with path.TempPipe() as tp:
+        DEFAULT_PY_LOCALS['context_pipe'] = tp.pipe_path
+        injected_locals.update(DEFAULT_PY_LOCALS)
+        command = '{};pipe=open(context_pipe, "w");pipe.write(json.dumps(context));pipe.close()'.format(command)
+        PyExec(command, injected_globals, injected_locals).start()
+        context = json.loads(tp.pipe.read())
     return context
 
 
