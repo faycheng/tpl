9f2e4aad6d3a4004e80378f44aa178b37dd6da57
faycheng <fei.cheng@daocloud.io>
2017-09-07 13:42:08 +0800

    add ShellExecError

---
+++ tpl/errors.py
@@ -0,0 +1,15 @@
+# -*- coding:utf-8 -*-
+
+from gettext import gettext as _
+
+
+class BaseError(BaseException):
+    ERR_MSG = _('')
+
+
+class ShellExecError(BaseError):
+    ERR_MSG = _('Command exit code not zero. \nExit Code:\n{}.\nOut:\n{}\nErr:\n{}')
+
+    def __init__(self, exit_code, out, err):
+        self.message = self.ERR_MSG.format(exit_code, out, err)
+        super(ShellExecError, self).__init__(self.message)
