82b20a3503d27e3f49d50d71c199da743d20f8a7
faycheng <fei.cheng@daocloud.io>
2017-09-08 13:42:40 +0800

    add recursion argument in list functions

--- tpl/path.py
+++ tpl/path.py
@@ -37,26 +37,38 @@ class TempPipe(object):
         os.remove(self.pipe_path)
 
 
-def list_dirs(path):
+def list_dirs(path, recursion=True):
     assert os.path.exists(path) and os.path.isdir(path)
-    for dir_path, dir_names, _ in os.walk(path):
-        for dir_name in dir_names:
-            yield os.path.join(dir_path, dir_name)
+    if recursion is True:
+        for dir_path, dir_names, _ in os.walk(path):
+            for dir_name in dir_names:
+                yield os.path.join(dir_path, dir_name)
+    if recursion is False:
+        for dir in [p for p in os.listdir(path) if os.path.isdir(p)]:
+            yield os.path.join(path, dir)
 
 
-def list_files(path):
+def list_files(path, recursion=True):
     assert os.path.exists(path) and os.path.isdir(path)
-    for dir_path, _, file_names in os.walk(path):
-        for file_name in file_names:
-            yield os.path.join(dir_path, file_name)
+    if recursion is True:
+        for dir_path, _, file_names in os.walk(path):
+            for file_name in file_names:
+                yield os.path.join(dir_path, file_name)
+    if recursion is False:
+        for file in [p for p in os.listdir(path) if os.path.isfile(p)]:
+            yield os.path.join(path, file)
 
 
-def list_all(path):
+def list_all(path, recursion=True):
     assert os.path.exists(path) and os.path.isdir(path)
-    for dir in list_dirs(path):
-        yield dir
-    for file in list_files(path):
-        yield file
+    if recursion is True:
+        for dir in list_dirs(path):
+            yield dir
+        for file in list_files(path):
+            yield file
+    if recursion is False:
+        for p in os.listdir(path):
+            yield os.path.join(path, p)
 
 
 def get_parent_path(path, depth=1):
