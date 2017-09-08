6bae1b7ad18710ca2b8e88c5c9285185329b56dd
faycheng <fei.cheng@daocloud.io>
2017-09-08 15:50:02 +0800

    render single file to template_res

--- examples/single_file/main.py
+++ examples/single_file/main.py
@@ -15,11 +15,14 @@ def main():
     cwd = path.get_parent_path(os.path.abspath(__file__), 1)
     tpl_dir = os.path.join(cwd, 'template')
     context = constructor.construct_context_from_shell(os.path.join(tpl_dir, 'construct.sh'))
-    t = Template(tpl_dir)
+    t = Template(tpl_dir, os.path.join(path.get_parent_path(os.path.abspath(__file__), 1), 'tpl_res'))
     render_dirs, render_files = t.render(context)
     for dir in render_dirs:
         path.mkdirs(dir)
     for file, file_content in render_files:
+        file_parent_dir = path.get_parent_path(file, 1)
+        if not os.path.exists(file_parent_dir):
+            path.mkdirs(file_parent_dir)
         with open(file, 'w') as fd:
             fd.write(file_content)
 
