9cce47d37f6e2d08a66b9deedfc6f2f74b02720a
程飞 <fay.cheng.cn@gmail.com>
2017-09-09 17:19:32 +0800

    add int validator

--- tpl/prompt/validator.py
+++ tpl/prompt/validator.py
@@ -8,5 +8,14 @@ class StrValidator(Validator):
         pass
 
 
+class IntValidator(Validator):
+    def validate(self, document):
+        text = document.text
+        for index, char in enumerate(text):
+            if not char.isdigit():
+                raise ValidationError(message='Input contains non-numeric char', cursor_position=index)
+
+
+
 
 
