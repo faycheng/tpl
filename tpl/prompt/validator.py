127818fea683f578d83575f944f6c49332954003
程飞 <fay.cheng.cn@gmail.com>
2017-09-09 17:19:11 +0800

    add str validator

--- tpl/prompt/validator.py
+++ tpl/prompt/validator.py
@@ -3,11 +3,10 @@
 from prompt_toolkit.validation import Validator, ValidationError
 
 
-class NumberValidator(Validator):
+class StrValidator(Validator):
     def validate(self, document):
-        text = document.text
-        if not (text and text.isdigit):
-            return
-        for index, char in enumerate(text):
-            if not char.isdigit():
-                raise ValidationError(message='Input contains non-numeric char', cursor_position=index)
+        pass
+
+
+
+
