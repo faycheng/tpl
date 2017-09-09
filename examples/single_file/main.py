c9356ff1149d0eb84b9a04d4c9959be541058685
程飞 <fay.cheng.cn@gmail.com>
2017-09-09 16:17:42 +0800

    rename: tpl.py -> core.py

--- examples/single_file/main.py
+++ examples/single_file/main.py
@@ -6,7 +6,7 @@ import sys
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
 
 
-from tpl.tpl import Template
+from tpl.core import Template
 from tpl import path
 from tpl import constructor
 
