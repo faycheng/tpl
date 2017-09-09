79df2ff4c9055742cd596c46d900b91b4202876b
程飞 <fay.cheng.cn@gmail.com>
2017-09-09 16:39:05 +0800

    add type convertion on prompt

--- tpl/prompt/prompt.py
+++ tpl/prompt/prompt.py
@@ -2,6 +2,7 @@
 
 import os
 
+from enum import Enum
 import prompt_toolkit
 
 from prompt_toolkit.history import FileHistory
@@ -11,21 +12,34 @@ from .completer import *
 history = FileHistory(os.path.join(path.HOME, '.templates', 'tpl.history'))
 
 
-def prompt(message, default=None, multiline=False):
+class PromptType(Enum):
+    STR = str
+    INT = int
+    BOOL = bool
+    FLOAT = float
+    LIST = list
+    DICT = dict
+    DIR = str
+    FILE = str
+
+
+def prompt(message, type='STR', default=None, multiline=False):
+    assert hasattr(PromptType, type)
     completer = WordCompleter(words=[], history=history)
     res = prompt_toolkit.prompt(message, default=default or '', history=history, completer=completer, multiline=multiline)
-    return str(res)
+    return getattr(PromptType, type)(res)
 
 
-def prompt_list(message, default=None, completions=None, multiline=False):
+def prompt_list(message, type='STR', default=None, completions=None, multiline=False):
+    assert hasattr(PromptType, type)
     completer = WordCompleter(words=completions, history=history)
     res = prompt_toolkit.prompt(message, default=default or '', history=history, completer=completer, multiline=multiline)
-    return res
+    return getattr(PromptType, type)(res)
 
 
 def prompt_path(message, root, type='DIR', recursion=False, default=None):
-    completer = PathCompleter(root, match_type=type, recursion=False)
+    assert hasattr(PromptType, type)
+    completer = PathCompleter(root, match_type=type, recursion=recursion)
     res = prompt_toolkit.prompt(message, default=default or '', completer=completer)
-    return res
-
+    return getattr(PromptType, type)(res)
 
