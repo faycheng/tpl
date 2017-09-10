b493f5cef96f8d0d8a4dc605c683122771823035
程飞 <fay.cheng.cn@gmail.com>
2017-09-10 15:22:19 +0800

    remove py execute

--- tpl/sandbox.py
+++ tpl/sandbox.py
@@ -29,30 +29,6 @@ DEFAULT_SHELL_VARS = {
 }
 
 
-class PyExec(threading.Thread):
-    def __init__(self, command, injected_globals, injected_locals):
-        self.command = command
-        self.injected_globals = injected_globals
-        self.injected_locals = injected_locals
-        super(PyExec, self).__init__(name='py_exec_tpl_constructor')
-
-    def run(self):
-        exec(self.command, self.injected_globals, self.injected_locals)
-
-
-def py_execute(command, injected_globals=None, injected_locals=None):
-    injected_globals = injected_globals or {}
-    injected_locals = injected_locals or {}
-    injected_globals.update(DEFAULT_PY_GLOBALS)
-    with path.TempPipe() as tp:
-        DEFAULT_PY_LOCALS['context_pipe'] = tp.pipe_path
-        injected_locals.update(DEFAULT_PY_LOCALS)
-        command = 'context={};pipe=open(context_pipe, "w");pipe.write(json.dumps(context));pipe.close()'.format(command)
-        PyExec(command, injected_globals, injected_locals).start()
-        context = json.loads(tp.pipe.read())
-    return context
-
-
 class ShellExec(threading.Thread):
     def __init__(self, command, injected_vars=None):
         self.command = command
