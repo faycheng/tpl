22ceb8fa256c4fb277ef3317984f1c56a461af0d
程飞 <fay.cheng.cn@gmail.com>
2017-09-10 15:27:25 +0800

    add test case of shell execute

--- tests/test_sandbox.py
+++ tests/test_sandbox.py
@@ -2,16 +2,10 @@ import pytest
 
 from unittest import mock
 from .faker import *
-from tpl.sandbox import py_execute, shell_execute
-
-
-def test_py_execute():
-    command = '{"key":"value"}'
-    context = py_execute(command)
-    assert isinstance(context, dict)
-    assert context.get('key') == 'value'
-
+from tpl.sandbox import shell_execute
 
 
 def test_shell_execute():
-    pass
+    command = """echo '{"key":"value"}' >> $pipe"""
+    context = shell_execute(command)
+    assert isinstance(context, dict)
