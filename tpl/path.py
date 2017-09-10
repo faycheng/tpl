2ac05a8ccc9d7a9ab4f455f18355d80af9e13c84
程飞 <fay.cheng.cn@gmail.com>
2017-09-10 18:11:47 +0800

    add temp file context

--- tpl/path.py
+++ tpl/path.py
@@ -14,7 +14,28 @@ class TempDir(object):
 
 
 class TempFile(object):
-    pass
+    def __init__(self, name=None, suffix='tmp'):
+        self.path = '/tmp/{}.{}'.format(name or str(uuid.uuid4()), suffix)
+        self._fd = None
+        self._close = False
+
+    @property
+    def fd(self):
+        if self._fd is None:
+            self._fd = open(self.path, 'w')
+        return self._fd
+
+    def close(self):
+        if self._close is True:
+            return
+        self.fd.close()
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.close()
+        os.remove(self.path)
 
 
 class TempPipe(object):
